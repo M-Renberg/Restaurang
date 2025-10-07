3b0b7e84b50fe66a6c31da2bfb751f57c01428df
M-Renberg <mikael.k.renberg@hotmail.com>
2025-10-07 08:41:40 +0200

    Login
    started working on the log in function

--- Program.cs
+++ Program.cs
@@ -1,3 +1,59 @@
-﻿using RestaurangApp;
+﻿using System.ComponentModel.Design;
+using RestaurangApp;
+
+FileHandler fh = new FileHandler();
 
 List<User> staffList = new List<User>();
+
+//test admin
+User Admin = new User("admin", "admin");
+staffList.Add(Admin);
+Admin.GetRole();
+
+fh.LoadUser(staffList);
+
+User? activeUser = null; 
+bool running = true;
+
+while (running)
+{
+
+    if (activeUser == null)
+    {
+        System.Console.WriteLine("Restaurang Management System:");
+        System.Console.WriteLine(" ");
+
+        string? inputLoginMenu = Console.ReadLine();
+
+        switch (inputLoginMenu)
+        {
+            case "1":
+                System.Console.WriteLine("Enter you username");
+                string? loginUsername = Console.ReadLine();
+                System.Console.WriteLine("Enter you password");
+                string? loginPassword = Console.ReadLine();
+
+                foreach (User user in staffList)
+                {
+                    if (user.TryLogin(loginUsername, loginPassword))
+                    {
+                        activeUser = user;
+                        break;
+                    }
+                }
+
+
+                break;
+            default:
+                System.Console.WriteLine("Wrong input. Please try again");
+                break;
+        }
+    }
+
+    else
+    {
+        
+        
+    }
+
+}
